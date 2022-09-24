9d582d68ef4f6d938efe588f72f44ccb5d0bcc2e
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-24 17:50:18 -0300

    Criando modelo de dados DTO TelescopeSpaceDTO

---
+++ src/main/java/com/api/telecopeEspace/dto/TelescopeSpaceDTO.java
@@ -0,0 +1,82 @@
+package com.api.telecopeEspace.dto;
+
+public class TelescopeSpaceDTO {
+
+	private Long id;
+	private String nome;
+	private String img;
+	private String tipo;
+	private String missao;
+	private String durationMissao;
+	private String dataDelancamento;
+	private String status;
+
+	public TelescopeSpaceDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getMissao() {
+		return missao;
+	}
+
+	public void setMissao(String missao) {
+		this.missao = missao;
+	}
+
+	public String getDurationMissao() {
+		return durationMissao;
+	}
+
+	public void setDurationMissao(String durationMissao) {
+		this.durationMissao = durationMissao;
+	}
+
+	public String getDataDelancamento() {
+		return dataDelancamento;
+	}
+
+	public void setDataDelancamento(String dataDelancamento) {
+		this.dataDelancamento = dataDelancamento;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
