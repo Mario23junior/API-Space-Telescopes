0aa6cbc035ac8a3c5ce4abf06f15de051c89e978
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-24 17:44:00 -0300

    Criando o modelo de dados DTO  EspecificacaoOrbitalDTO
    O objetivo e modularizar os dados de transferência.

---
+++ src/main/java/com/api/telecopeEspace/dto/EspecificacaoOrbitalDTO.java
@@ -0,0 +1,67 @@
+package com.api.telecopeEspace.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class EspecificacaoOrbitalDTO {
+
+	private Long id;
+	private String decaimento;
+	private double excentricidadeOrbital;
+	private String referenciaOrbital;
+	private String periodoOrbital;
+	private Double inclinacaoOrbital;
+
+	public EspecificacaoOrbitalDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDecaimento() {
+		return decaimento;
+	}
+
+	public void setDecaimento(String decaimento) {
+		this.decaimento = decaimento;
+	}
+
+	public double getExcentricidadeOrbital() {
+		return excentricidadeOrbital;
+	}
+
+	public void setExcentricidadeOrbital(double excentricidadeOrbital) {
+		this.excentricidadeOrbital = excentricidadeOrbital;
+	}
+
+	public String getReferenciaOrbital() {
+		return referenciaOrbital;
+	}
+
+	public void setReferenciaOrbital(String referenciaOrbital) {
+		this.referenciaOrbital = referenciaOrbital;
+	}
+
+	public String getPeriodoOrbital() {
+		return periodoOrbital;
+	}
+
+	public void setPeriodoOrbital(String periodoOrbital) {
+		this.periodoOrbital = periodoOrbital;
+	}
+
+	public Double getInclinacaoOrbital() {
+		return inclinacaoOrbital;
+	}
+
+	public void setInclinacaoOrbital(Double inclinacaoOrbital) {
+		this.inclinacaoOrbital = inclinacaoOrbital;
+	}
+
+}
