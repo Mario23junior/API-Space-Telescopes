ff652ec13ba5889acc4182c150a021cff7e17718
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-24 15:25:16 -0300

    Criando modelo de dados de EspecificacaoOrbital

---
+++ src/main/java/com/api/telecopeEspace/model/EspecificacaoOrbital.java
@@ -0,0 +1,72 @@
+package com.api.telecopeEspace.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class EspecificacaoOrbital {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String decaimento;
+	private double excentricidadeOrbital;
+	private String referenciaOrbital;
+	private String periodoOrbital;
+	private Double inclinacaoOrbital;
+
+	public EspecificacaoOrbital() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDecaimento() {
+		return decaimento;
+	}
+
+	public void setDecaimento(String decaimento) {
+		this.decaimento = decaimento;
+	}
+
+	public double getExcentricidadeOrbital() {
+		return excentricidadeOrbital;
+	}
+
+	public void setExcentricidadeOrbital(double excentricidadeOrbital) {
+		this.excentricidadeOrbital = excentricidadeOrbital;
+	}
+
+	public String getReferenciaOrbital() {
+		return referenciaOrbital;
+	}
+
+	public void setReferenciaOrbital(String referenciaOrbital) {
+		this.referenciaOrbital = referenciaOrbital;
+	}
+
+	public String getPeriodoOrbital() {
+		return periodoOrbital;
+	}
+
+	public void setPeriodoOrbital(String periodoOrbital) {
+		this.periodoOrbital = periodoOrbital;
+	}
+
+	public Double getInclinacaoOrbital() {
+		return inclinacaoOrbital;
+	}
+
+	public void setInclinacaoOrbital(Double inclinacaoOrbital) {
+		this.inclinacaoOrbital = inclinacaoOrbital;
+	}
+
+}
