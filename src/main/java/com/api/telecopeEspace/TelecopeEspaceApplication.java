9c289219835e45d1952f42358c9e7ebda67c28b7
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-24 13:57:03 -0300

    Inciando projeto de listagem de telecopios espaciais

---
+++ src/main/java/com/api/telecopeEspace/TelecopeEspaceApplication.java
@@ -0,0 +1,13 @@
+package com.api.telecopeEspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecopeEspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecopeEspaceApplication.class, args);
+	}
+
+}
