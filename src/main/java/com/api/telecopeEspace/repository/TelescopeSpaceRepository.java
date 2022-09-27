cb1bbf84096cb8258b9d7f796971fda26b1f99a7
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-27 00:54:00 -0300

    Criando classe de tratamento de erro

--- src/main/java/com/api/telecopeEspace/repository/TelescopeSpaceRepository.java
+++ src/main/java/com/api/telecopeEspace/repository/TelescopeSpaceRepository.java
@@ -1,9 +1,12 @@
 package com.api.telecopeEspace.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.telecopeEspace.model.TelescopeSpace;
 
 public interface TelescopeSpaceRepository extends JpaRepository<TelescopeSpace, Long> {
-
+   
+	List<TelescopeSpace> findByNome (String nome);
 }
