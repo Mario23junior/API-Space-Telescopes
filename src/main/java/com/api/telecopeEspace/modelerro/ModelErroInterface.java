2e12531973658361fed13e7ea8d8836cfaf8b2ad
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-27 01:12:27 -0300

    Criando modelo de dados de retorno de erro

---
+++ src/main/java/com/api/telecopeEspace/modelerro/ModelErroInterface.java
@@ -0,0 +1,59 @@
+package com.api.telecopeEspace.modelerro;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ModelErroInterface {
+
+	private Integer status;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy/MM/yyyy")
+	private Date timestap;
+	private String message;
+	private String path;
+
+	public ModelErroInterface() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ModelErroInterface(Integer status, Date timestap, String message, String path) {
+		super();
+		this.status = status;
+		this.timestap = timestap;
+		this.message = message;
+		this.path = path;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Date getTimestap() {
+		return timestap;
+	}
+
+	public void setTimestap(Date timestap) {
+		this.timestap = timestap;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
